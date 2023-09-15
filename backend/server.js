c68cfba715f1fe4e295b02bc17c7b2b98c6c14e7
Faraz shafi <95197056+farazshafi@users.noreply.github.com>
2023-09-15 10:26:01 +0530

    changed endpoint and origin to url

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server = app.listen(port, console.log(`Server started on port ${port}`.yel
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://chaatiko.onrender.com/"
     }
 })
 
--- frontend/src/Components/SingleChat.js
+++ frontend/src/Components/SingleChat.js
@@ -12,7 +12,7 @@ import io from "socket.io-client"
 import Lottie from "lottie-react"
 import animationData from "../animations/typing.json"
 
-const ENDPOINT = "http://chaatiko.onrender.com"
+const ENDPOINT = "https://chaatiko.onrender.com/"
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
